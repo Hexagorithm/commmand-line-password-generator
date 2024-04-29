6a4af3c4c0cfefd55b9d711f41dc6b996878e9a6
Hexagoritm <139809645+Hexagorithm@users.noreply.github.com>
2024-04-29 19:11:58 +0200

    Make immaculate plan

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* Password generator*/ 
+
+/* should fill empty space with not specified characters*/
+/* should append chars, then symbols, then numbers (if want good password, see -m) */
+
+
+/* Functionality:
+ * length "-l <int>" (check if input valid with length)
+ * number of alphabet chars "-a <int>"
+ * number of numbers "-n <int>"
+ * number of special symbols "-s <int>"
+ * randomize case "-rc"
+ * mix "-m"
+*/
