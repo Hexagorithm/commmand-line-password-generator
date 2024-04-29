a49e480bc98177f4e30e3a7c82437c509de067ec
Hexagoritm <139809645+Hexagorithm@users.noreply.github.com>
2024-04-29 19:19:15 +0200

    Add usage output

--- main.c
+++ main.c
@@ -16,3 +16,21 @@
  * use defaults "-d"
 */
 
+
+int main(int argc, char* argv[])
+{
+	if (argc == 1)
+	{
+		printf(
+				"Create password:\n"
+				"\t-l <int> -> specify length\n"
+				"\t-a <int> -> specify number of alpha chars\n"
+				"\t-n <int> -> specify number of digits\n"
+				"\t-s <int> -> specify number of special chars\n"
+				"\t-rc -> randomize case of alpha chars (default is all lower)\n"
+				"\t-m -> mix password (default password: <alpha><symb><numbers>)\n"
+				"\t-d -> default password (-l, -a, -n not allowed)\n"
+				);
+		return 0;
+	}
+}
