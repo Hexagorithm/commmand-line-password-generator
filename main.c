6033005cb66c423742b938e2ba0c11a80e7aae89
Hexagoritm <139809645+Hexagorithm@users.noreply.github.com>
2024-04-30 14:00:24 +0200

    Change -rc flag to -r (so all flags are 1 char long)

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
  * number of alphabet chars "-a <int>"
  * number of numbers "-n <int>"
  * number of special symbols "-s <int>"
- * randomize case "-rc"
+ * randomize case "-r"
  * mix "-m"
  * use defaults "-d"
 */
@@ -51,7 +51,7 @@ int main(int argc, char* argv[])
 				"\t-a <int> -> specify number of alpha chars\n"
 				"\t-n <int> -> specify number of digits\n"
 				"\t-s <int> -> specify number of special chars\n"
-				"\t-rc -> randomize case of alpha chars (default is all lower)\n"
+				"\t-r -> randomize case of alpha chars (default is all lower)\n"
 				"\t-m -> mix password (default password: <alpha><symb><numbers>)\n"
 				"\t-d -> default password (-l, -a, -n not allowed)\n"
 				);
